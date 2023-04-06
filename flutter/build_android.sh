6382f650e245fe2192549f111d51d0a24a43c38f
junbinhe <hejunbin@gmail.com>
2023-04-06 18:48:51 +0800

    Update build_android.sh

--- flutter/build_android.sh
+++ flutter/build_android.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 MODE=${MODE:=release}
-$ANDROID_NDK_HOME/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-strip android/app/src/main/jniLibs/arm64-v8a/*
+$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip android/app/src/main/jniLibs/arm64-v8a/*
 flutter build apk --target-platform android-arm64,android-arm --${MODE} --obfuscate --split-debug-info ./split-debug-info
 flutter build apk --split-per-abi --target-platform android-arm64,android-arm --${MODE} --obfuscate --split-debug-info ./split-debug-info
 flutter build appbundle --target-platform  android-arm64,android-arm --${MODE} --obfuscate --split-debug-info ./split-debug-info
